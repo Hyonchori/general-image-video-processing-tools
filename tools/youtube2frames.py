2d6ee5f29b3663e5bb99e9df71c9ab441badc049
root <gus8cjf@naver.com>
2022-03-03 14:15:30 +0900

    save youtube video into frame images or video

--- tools/youtube2frames.py
+++ tools/youtube2frames.py
@@ -1,8 +1,9 @@
-# Load youtube video and save as frames
+# Load youtube video and save as frames or video
 import argparse
 import os
 import sys
 from pathlib import Path
+from datetime import timedelta
 
 import cv2
 
@@ -15,9 +16,12 @@ from custom_utils.general import increment_path, get_youtube_stream
 
 def main(args):
     source = args.source
+    start_time = args.start_time
+    end_time = args.end_time
     save_dir = args.save_dir
     target_size = args.target_size
     view = args.view
+    save = args.save
     save_mode = args.save_mode
     assert save_mode in [1, 2], f"Save mode should be in [1(images), 2(video)]"
 
@@ -32,13 +36,40 @@ def main(args):
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    total_seconds = int(cap.get(cv2.CAP_PROP_FRAME_COUNT) / fps)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    total_seconds = int(total_frames / fps)
     tmp_frame1 = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
     if save_mode == 2:
         save_path = os.path.join(save_dir, f"{run_name}.mp4")
         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, target_size)
 
-    print(f"\twidth: {width}, height: {height}, fps: {fps:.2f}, duration: {total_seconds // 60}:{total_seconds % 60}")
+    if start_time is None:
+        start_frame = 0
+    else:
+        assert all(isinstance(x, int) for x in start_time), "All elements in start_time should be int"
+        start_time_delta = timedelta(hours=start_time[0], minutes=start_time[1], seconds=start_time[2])
+        total_time_delta = timedelta(seconds=total_seconds)
+        start_time_rate = min(1, max(0, start_time_delta / total_time_delta))
+        start_frame = int(total_frames * start_time_rate)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+    if end_time is None:
+        end_frame = total_frames
+    else:
+        assert all(isinstance(x, int) for x in end_time), "All elements in end_time should be int"
+        if any(x < 0 for x in end_time):
+            end_frame = total_frames
+        else:
+            end_time_delta = timedelta(hours=end_time[0], minutes=end_time[1], seconds=end_time[2])
+            total_time_delta = timedelta(seconds=total_seconds)
+            end_time_rate = min(1, max(0, end_time_delta / total_time_delta))
+            end_frame = int(total_frames * end_time_rate)
+            assert end_frame >= start_frame, "end_time should be bigger than start time"
+
+    print(f"\twidth: {width}, height: {height}, fps: {fps:.2f}, " +
+          f"duration: {total_seconds // 60}:{total_seconds % 60}, total_frames: {total_frames}\n" +
+          f"\tstart_time: {start_time_delta}, end_time: {end_time_delta}")
+
     while True:
         ret, img = cap.read()
         if not ret:
@@ -49,25 +80,35 @@ def main(args):
         else:
             tmp_frame1 = tmp_frame2
         img = cv2.resize(img, dsize=target_size)
-        if save_mode == 1:
-            save_path = os.path.join(save_dir, f"{run_name}_{tmp_frame1}.png")
-            cv2.imwrite(save_path, img)
-        else:  # save_mode == 2:
-            vid_writer.write(img)
+        if save:
+            if save_mode == 1:
+                save_path = os.path.join(save_dir, f"{run_name}_{tmp_frame1}.png")
+                cv2.imwrite(save_path, img)
+            else:  # save_mode == 2:
+                vid_writer.write(img)
         if view:
             cv2.imshow("img", img)
             cv2.waitKey(1)
-    if save_mode == 1:
-        print(f"\ttotal {tmp_frame1} images are saved!")
-    else:
-        print(f"total video is saved!")
+        if tmp_frame1 == end_frame:
+            break
+    if save:
+        if save_mode == 1:
+            print(f"\ttotal {tmp_frame1} images are saved!")
+        else:
+            print(f"total video is saved!")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    source = "https://www.youtube.com/watch?v=cDD2ToN10KI"
+    source = "https://www.youtube.com/watch?v=P8dgrReTkrU"
     parser.add_argument("--source", type=str, default=source)
+
+    start_time = [0, 0, 59]
+    parser.add_argument("--start-time", type=int, default=start_time)
+
+    end_time = [0, 1, 11]
+    parser.add_argument("--end-time", type=int, default=end_time)
 
     save_dir = f"{FILE.parents[1]}/youtube_frames"
     parser.add_argument("--save-dir", type=str, default=save_dir)
@@ -76,6 +117,7 @@ def parse_args():
     parser.add_argument("--target-size", type=int, default=target_size)
 
     parser.add_argument("--view", action="store_true", default=True)
+    parser.add_argument("--save", action="store_true", default=False)
     parser.add_argument("--save-mode", type=int, default=2)  # 1: images, 2: video
 
     args = parser.parse_args()
